5abe0e49412d4443deb6730d55edc2b1adb28f13
Jeremy FRAZIER <frazierjeremi@gmail.com>
2023-12-13 16:32:47 +0100

    tableau carte

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,30 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 let nbPlayers = 0;
+let initialcards :number[] = [];
+
+
 
 
 io.on('connection', (socket) => { 
   nbPlayers++;
   if(nbPlayers === 2)  {
     io.emit('partieprete');
-    console.log('partieprete');
+    console.log('partieprete')
   }
-  // socket.on('demandesynchro', (msg) => {
-  //   console.log('demandesynchro' + msg);
-  })
+  const shuffle = (initialcards: string[]) => {
+      for (let i = 1; i < 101; i++) {
+        initialcards.push(i);
+        
+      }
+
+  }
+
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
     socket.broadcast.emit('patate', msg)
   });
-// });
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
