3a125e5fad1f8d49f92c2e2a93f93361501c7f66
Jeremy FRAZIER <frazierjeremi@gmail.com>
2023-12-13 16:55:13 +0100

    debut players

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { Socket } from 'socket.io-client';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 let nbPlayers = 0;
 let initialcards :number[] = [];
-
+let Players: any[] = []
 
 
 
 io.on('connection', (socket) => { 
-  nbPlayers++;
-  if(nbPlayers === 2)  {
+  Players.push(socket)
+  if(Players.length === 2)  {
     io.emit('partieprete');
     console.log('partieprete')
-  }
-  const shuffle = (initialcards: string[]) => {
-      for (let i = 1; i < 101; i++) {
-        initialcards.push(i);
-        
-      }
-
+    for (let i = 1; i < 101; i++) {
+      initialcards.push(i);
+    
+    }
   }
 
   socket.on('chat message', (msg) => {
