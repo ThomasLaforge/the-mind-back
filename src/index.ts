54511bc844f513cf20d15a47f1fea2ce16369037
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-12-14 11:54:04 +0100

    start game

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,47 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import "dotenv/config";
+
+const PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-let nbPlayers = 0;
 
+let players: Socket[] = [];
+let nbTurn = 1;
+let nbLife = 2;
+let nbSShuriken = 1;
+let cards: number[] = [];
+let lastCardValue = 0;
+
+for(let i=1; i<=100; i++) {
+  cards.push(i);
+}
 
 io.on('connection', (socket) => { 
-  nbPlayers++;
-  if(nbPlayers === 2)  {
-    socket.emit('partieprete');
+  players.push(socket);
+  
+  if(players.length === 2) {
+    io.emit('partieprete', nbLife, nbSShuriken, nbTurn);
+    players.forEach((socketPlayer, i) => {
+      const playerCards = cards.splice(i * nbTurn, i * nbTurn + nbTurn);
+      console.log('player cards at partieprete', playerCards);
+      socketPlayer.emit('initialcards', playerCards);
+    });
   }
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    socket.broadcast.emit('patate', msg)
+
+  socket.on('playcard', (cardValue) => {
+    const playerIndex = players.indexOf(socket);
+    if(cardValue > lastCardValue) {
+      lastCardValue = cardValue;
+      // Check if other players have a lower card
+      io.emit('cardplayed', cardValue);
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+server.listen(PORT, () => {
+  console.log('server running at http://localhost:' + PORT);
 });
